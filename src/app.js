f66ba1f9dba3c05ffa1a9cf228bc86b574bd19b3
Jaime B <j.barajas51801@gmail.com>
2025-07-09 14:07:44 -0500

    Updated html css and app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["my dog","my cat","my car","my relative" ];
+let action = ["ate","drove","filed", "fixed my tv"];
+let what = ["my sandwhich","my car","my taxes","my tv"];
+let when = ["last week","last year","yesterday","3 days ago"];
+
+function randomfrom(array)
+{
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+
+function genexcuse(){
+    const excuse = `${randomfrom(who)} ${randomfrom(action)} ${randomfrom(what)} ${randomfrom(when)}`
+    document.getElementById("excuse").textContent = excuse;
+}
+
+genexcuse()
